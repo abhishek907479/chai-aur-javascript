43993c999e29bfe63aaf89f845d3818e4a10c102
abhishek907479 <abhisheksathyan483@gmail.com>
2024-12-14 16:07:36 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,17 @@
+const accountId = 1445553
+let accountEmail = "abhishek@gmail.com"
+var accountPassword =  "907479"
+
+acountCity="jaipur"
+
+ // accountId = 3   not allowed
+
+
+ accountEmail = "hchc@gmail.com"
+ accountPassword = "453535"
+
+
+ console.log(accountEmail,accountPassword,acountCity);
+ 
+
+ //prefer not to use var because of issue in  block scope and functional scope
