de71f1c49f21d65c5d8e573e9f8d983e81d4d1d8
abhishek907479 <abhisheksathyan483@gmail.com>
2024-12-14 17:15:52 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,29 @@
+"use strict";  //treat all js code as newer version 
+
+
+// alert(3 + 4)   // we are using node js ,not browser
+
+
+let name =  "abhishek"
+let age = 19
+let isLoggedIn  = false
+
+
+//number ---> 2 to power 53
+//bigint
+//string ---> ""
+//booean --->true/false
+//null  ---> stand alone value
+//undefined
+//symbol --->unique
+
+//object
+
+console.log(typeof null);
+
+
+// type of null is an object
+
+console.log(typeof undefined)
+
+//type of undefined is undefined
