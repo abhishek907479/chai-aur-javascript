da77c8cf7067b28002b16fd564b3e0f868469de2
abhishek907479 <abhisheksathyan483@gmail.com>
2024-12-22 04:58:22 +0000

    conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,46 @@
+//let score="33"
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+//let value=Number(score)
+//console.log(typeof value)
+
+
+
+//let score="22abc"
+
+
+//console.log(typeof score)
+
+
+//let value=Number(score)
+
+// console.log(typeof value)
+//console.log(value)  // when the score is like "abc123" or "abc" ,,and convert it into number it will become NaN-- not a number.
+
+
+
+
+
+//"33"--> 33
+//"33abc"-->NaN 
+//true-->1
+//false-->0
+
+
+let isLoggedIn = "abhi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log( booleanIsLoggedIn)
+
+
+//1-->true
+//0-->false
+//""-->false
+//"abhi"-->true
+
+
+let number=33
+let StringNumber = String(number)
+console.log( typeof StringNumber)
